c728b50d8526d01f7a89f03ca3abd901adcadc70
Thomas Zachariah <tzachari@umich.edu>
2018-08-08 17:05:28 -0700

    Use new HTTP client library in PowerBlade parse app

--- gateway_parse_powerblade/main/parse_powerblade_main.c
+++ gateway_parse_powerblade/main/parse_powerblade_main.c
@@ -7,14 +7,15 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <math.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "apps/sntp/sntp.h"
-#include "lwip/netdb.h"
 #include "nvs_flash.h"
 #include "esp_bt.h"
 #include "esp_bt_main.h"
 #include "esp_gap_ble_api.h"
+#include "esp_http_client.h"
 #include "esp_event_loop.h"
 #include "esp_wifi.h"
 #include "esp_log.h"
@@ -22,25 +23,23 @@
 #define TAG "PARSE POWERBLADE"
 
 /* Endpoint for data */
-#define WEB_SERVER "webhook.site"
-#define WEB_PORT "80"
-#define WEB_URL "https://" WEB_SERVER "/f8e2db8b-4757-432c-9f87-53a618b15e91"
+#define WEB_URL "http://api.webhookinbox.com/i/j7bfVZ5k/in/"
 
 /* Structure of the POST request body to send parsed data*/
 #define BODY "{" \
-        "\"device\": \"PowerBlade\"," \
-        "\"id\": \"%s\"," \
-        "\"sequence_number\": \"%u\"," \
-        "\"rms_voltage\": \"%.2f\"," \
-        "\"power\": \"%.2f\"," \
-        "\"apparent_power\": \"%.2f\"," \
-        "\"energy\": \"%.2f\"," \
-        "\"power_factor\": \"%.2f\"," \
-        "\"_meta\": {" \
-            "\"received_time\": \"%s\"," \
-            "\"device_id\": \"%s\"," \
-            "\"receiver\": \"esp32-gateway\"," \
-            "\"gateway_id\": \"%s\"" \
+        "\"device\":\"PowerBlade\"," \
+        "\"id\":\"%s\"," \
+        "\"sequence_number\":\"%u\"," \
+        "\"rms_voltage\":\"%.2f\"," \
+        "\"power\":\"%.2f\"," \
+        "\"apparent_power\":\"%.2f\"," \
+        "\"energy\":\"%.2f\"," \
+        "\"power_factor\":\"%.2f\"," \
+        "\"_meta\":{" \
+            "\"received_time\":\"%s\"," \
+            "\"device_id\":\"%s\"," \
+            "\"receiver\":\"esp32-gateway\"," \
+            "\"gateway_id\":\"%s\"" \
         "}" \
     "}"
 
@@ -52,6 +51,8 @@
 /* Event group & bit to signal when Wi-Fi connected */
 static EventGroupHandle_t wifi_group;
 const int CONNECTED_BIT = BIT0;
+
+static int reset = 0;
 
 /* ID String variable & helper function */
 char gateway[17];
@@ -100,36 +101,29 @@ static void parse_data(char *device, char *iso, uint8_t *data, char *output) {
     sprintf(output, BODY, device, sequence_num, v_rms_out, real_power_out, app_power_out, watt_hours_out, pf_out, iso, device, gateway);
 }
 
-static void http_post(char *request) {
-    int s, r;
-    char recv_buf[64];
-    struct timeval receiving_timeout = { .tv_sec = 5, .tv_usec = 0 };
-    struct addrinfo hints = { .ai_family = AF_INET, .ai_socktype = SOCK_STREAM }, *res;
-    sprintf(request, "POST %s HTTP/1.1\r\nContent-Type: application/json\r\nContent-Length: %d\r\n\r\n%s", WEB_URL, strlen(request), request);
-    ESP_LOGI(TAG, "%s", request);
-    if ( !getaddrinfo(WEB_SERVER, WEB_PORT, &hints, &res) && res ) {
-        if ( (s = socket(res->ai_family,res->ai_socktype,0)) >= 0 ) {
-            if (!connect(s,res->ai_addr,res->ai_addrlen) && write(s, request, strlen(request)) >= 0 &&
-                 setsockopt(s,SOL_SOCKET,SO_RCVTIMEO,&receiving_timeout,sizeof(receiving_timeout)) >= 0 ) {
-                do {
-                    bzero(recv_buf, sizeof(recv_buf));
-                    r = read(s, recv_buf, sizeof(recv_buf)-1);
-                    for(int i = 0; i < r; i++) {
-                        putchar(recv_buf[i]);
-                    }
-                } while(r > 0);
-                putchar('\n');
-            } else {
-                ESP_LOGE(TAG, "Send failed");
+static void http_post(char *body) {
+    ESP_LOGI(TAG, "HTTP POST to %s: \n\n%s\n", WEB_URL, body);
+    esp_http_client_config_t config = { .url = WEB_URL };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_http_client_set_url(client, WEB_URL);
+    esp_http_client_set_method(client, HTTP_METHOD_POST);
+    esp_http_client_set_post_field(client, body, strlen(body));
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        int r;
+        char recv_buf[64];
+        ESP_LOGI(TAG, "HTTP POST Status = %d, Response:", esp_http_client_get_status_code(client));
+        do {
+            r = esp_http_client_read(client, recv_buf, sizeof(recv_buf)-1);
+            for (int i = 0; i < r; i++) {
+                putchar(recv_buf[i]);
             }
-            close(s);
-        } else {
-            ESP_LOGE(TAG, "Failed to allocate socket");
-        }
-        freeaddrinfo(res);
+        } while(r > 0);
+        putchar('\n');
     } else {
-        ESP_LOGE(TAG, "DNS lookup failed");
+        ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
     }
+    esp_http_client_cleanup(client);
 }
 
 static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* param) {
@@ -148,7 +142,7 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* par
             if (param->scan_rst.search_evt == ESP_GAP_SEARCH_INQ_RES_EVT) {
                 struct ble_scan_result_evt_param sr = param->scan_rst;
                 if (sr.bda[0]==0xC0 && sr.bda[1]==0x98 && sr.bda[2]==0xE5 && sr.bda[3]==0x70 && sr.adv_data_len>18) {
-                    char iso[20], device[17], request[512];
+                    char iso[20], device[17], body[512];
                     time_t now;
                     time(&now);
                     get_id_string(sr.bda, device);
@@ -157,11 +151,14 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* par
                     esp_log_buffer_hex("PARSE POWERBLADE: DATA", sr.ble_adv, sr.adv_data_len);
                     if (sr.ble_adv[3]<0x17 || sr.ble_adv[7]!=0x11 || sr.ble_adv[6] != 0x02 || sr.ble_adv[5] != 0xE0 || sr.ble_adv[8]!=0x02) {
                         ESP_LOGE(TAG,"No parseable data in this packet");
+                    } else if (now < 1500000000) {
+                        ESP_LOGE(TAG,"Still setting gateway time. Ignoring packet...");
                     } else {
-                        parse_data(device, iso, sr.ble_adv, request);
-                        http_post(request);
+                        parse_data(device, iso, sr.ble_adv, body);
+                        http_post(body);
                     }
                 }
+                reset = 0;
             }
             break;
         case ESP_GAP_BLE_SCAN_STOP_COMPLETE_EVT:
@@ -173,6 +170,17 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* par
             break;
         default:
             break;
+    }
+}
+
+void reset_task(void *pvParameter) {
+    while(1) {
+        if (reset) {
+            esp_restart();
+        } else {
+            reset = 1;
+            vTaskDelay(15000 / portTICK_PERIOD_MS);
+        }
     }
 }
 
@@ -212,4 +220,7 @@ void app_main() {
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
     sntp_setservername(0, "pool.ntp.org");
     sntp_init();
+
+    /* Initiate reset task */
+    xTaskCreate(&reset_task, "reset_task", configMINIMAL_STACK_SIZE, NULL, 10, NULL);
 }
