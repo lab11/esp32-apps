720e54c91d912f82312a2af0b167d68125c6b949
Thomas Zachariah <tzachari@umich.edu>
2018-09-10 14:07:25 -0700

    Working Influx example

--- gateway_powerblade_influx/main/powerblade_influx_main.c
+++ gateway_powerblade_influx/main/powerblade_influx_main.c
@@ -20,28 +20,24 @@
 #include "esp_wifi.h"
 #include "esp_log.h"
 
-#define TAG "PARSE POWERBLADE"
+#define TAG "POWERBLADE INFLUX"
 
 /* Endpoint for data */
-#define WEB_URL "http://api.webhookinbox.com/i/CjHOjT40/in/"
+#define URL CONFIG_HOST "/write?db=" CONFIG_DB
 
-/* Structure of the POST request body to send parsed data*/
-#define BODY "%s{" \
-        "\"device\":\"PowerBlade\"," \
-        "\"id\":\"%s\"," \
-        "\"sequence_number\":\"%u\"," \
-        "\"rms_voltage\":\"%.2f\"," \
-        "\"power\":\"%.2f\"," \
-        "\"apparent_power\":\"%.2f\"," \
-        "\"energy\":\"%.2f\"," \
-        "\"power_factor\":\"%.2f\"," \
-        "\"_meta\":{" \
-            "\"received_time\":\"%s\"," \
-            "\"device_id\":\"%s\"," \
-            "\"receiver\":\"esp32-gateway\"," \
-            "\"gateway_id\":\"%s\"" \
-        "}" \
-    "}%s"
+/* Structure of the data to be sent */
+#define DAT "%sdata," \
+            "device_class=\"PowerBlade\"," \
+            "device_id=\"%s\"," \
+            "gateway_id=\"%s\"," \
+            "receiver=\"esp32-gateway\" " \
+            "sequence_number=%u," \
+            "rms_voltage=%.2f," \
+            "power=%.2f," \
+            "apparent_power=%.2f," \
+            "energy=%.2f," \
+            "power_factor=%.2f " \
+            "%ld%06ld\n"
 
 /* Leave as is to set Wi-Fi configuration using 'make menuconfig'
    Or set it below - ie #define WIFI_SSID "mywifissid" */
@@ -53,13 +49,14 @@ static EventGroupHandle_t wifi_group;
 const int CONNECTED_BIT = BIT0;
 
 /* Strings */
-static char body[32768], recv_buf[64], iso[20], gateway[17], id[17];
+static char body[32768], recv_buf[64], gateway[17], id[17];
 
 /* Struct for parsed data */
 typedef struct {
     uint8_t device[ESP_BD_ADDR_LEN];
     uint32_t seq_num;
-    time_t time_received;
+    time_t sec;
+    suseconds_t usec;
     double v_rms;
     double real_power;
     double app_power;
@@ -95,7 +92,7 @@ static esp_err_t wifi_event_handler(void *ctx, system_event_t *event) {
     return ESP_OK;
 }
 
-static void parse_data(esp_bd_addr_t device, time_t now, uint8_t *data) {
+static void parse_data(esp_bd_addr_t device, struct timeval tv, uint8_t *data) {
     /* Example AD: 02 01 06 17 ff e0 02 11 02 00 54 8a 1d 4f ff 79 09 c8 0c 83 0c b7 01 11 98 ba 42 */
     uint32_t sequence_num   = data[9]<<24 | data[10]<<16 | data[11]<<8 | data[12];
     uint32_t pscale         = data[13]<<8 | data[14];
@@ -109,7 +106,8 @@ static void parse_data(esp_bd_addr_t device, time_t now, uint8_t *data) {
     double power_scale      = (pscale & 0x0FFF) / pow(10.0, (pscale & 0xF000)>>12);
     size_t n                = items_length++;
     items[n].seq_num        = sequence_num;
-    items[n].time_received  = now;
+    items[n].sec            = tv.tv_sec;
+    items[n].usec           = tv.tv_usec;
     items[n].v_rms          = v_rms * volt_scale;
     items[n].real_power     = real_power * power_scale;
     items[n].app_power      = apparent_power * power_scale;
@@ -122,27 +120,25 @@ static void http_post_task() {
     while (1) {
         vTaskDelay( 1000 / portTICK_PERIOD_MS);
         if (!items_length) { continue; };
-        esp_http_client_config_t config = { .url = WEB_URL };
+        esp_http_client_config_t config = { .url = URL };
         esp_http_client_handle_t client = esp_http_client_init(&config);
 
-        sprintf(body, "{\"items\":[");
+        body[0] = 0;
         for (int n=0; n<items_length; n++) {
             get_id_string(items[n].device, id);
-            strftime(iso, sizeof iso, "%FT%TZ", gmtime(&(items[n].time_received)));
-            sprintf(body, BODY, body, id, items[n].seq_num, items[n].v_rms, items[n].real_power, items[n].app_power, items[n].watt_hours, items[n].pf, iso, id, gateway, n==items_length-1?"":",");
+            sprintf(body, DAT, body, id, gateway, items[n].seq_num, items[n].v_rms, items[n].real_power, items[n].app_power, items[n].watt_hours, items[n].pf, items[n].sec, items[n].usec);
         }
-        sprintf(body,"%s]}",body);
-        ESP_LOGI(TAG, "HTTP POST to %s: \n\n%s\n", WEB_URL, body);
+        items_length = 0;
+        ESP_LOGI(TAG, "HTTP POST to %s: \n\n%s\n", URL, body);
 
-
-        esp_http_client_set_url(client, WEB_URL);
+        esp_http_client_set_url(client, URL);
         esp_http_client_set_method(client, HTTP_METHOD_POST);
-        esp_http_client_set_header(client, "Content-Type", "application/json");
         esp_http_client_set_post_field(client, body, strlen(body));
         esp_err_t err = esp_http_client_perform(client);
+
         if (err == ESP_OK) {
             int r;
-            items_length = 0;
+            printf("RESET %u\n",items_length);
             ESP_LOGI(TAG, "HTTP POST Status = %d, Response:", esp_http_client_get_status_code(client));
             do {
                 r = esp_http_client_read(client, recv_buf, sizeof(recv_buf)-1);
@@ -174,16 +170,16 @@ static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* par
             if (param->scan_rst.search_evt == ESP_GAP_SEARCH_INQ_RES_EVT) {
                 struct ble_scan_result_evt_param sr = param->scan_rst;
                 if (sr.bda[0]==0xC0 && sr.bda[1]==0x98 && sr.bda[2]==0xE5 && sr.bda[3]==0x70 && sr.adv_data_len>18) {
-                    time_t now;
-                    time(&now);
-                    esp_log_buffer_hex("\nPARSE POWERBLADE: >>>>>>> FOUND DEVICE", sr.bda, 6);
-                    esp_log_buffer_hex("PARSE POWERBLADE: DATA", sr.ble_adv, sr.adv_data_len);
+                    struct timeval tv = {0, 0};
+                    gettimeofday(&tv, NULL);
+                    esp_log_buffer_hex("\nFOUND DEVICE", sr.bda, 6);
+                    esp_log_buffer_hex("DATA", sr.ble_adv, sr.adv_data_len);
                     if (sr.ble_adv[3]<0x17 || sr.ble_adv[7]!=0x11 || sr.ble_adv[6] != 0x02 || sr.ble_adv[5] != 0xE0 || sr.ble_adv[8]!=0x02) {
                         ESP_LOGE(TAG,"No parseable data in this packet");
-                    } else if (now < 1500000000) {
+                    } else if (tv.tv_sec < 1500000000) {
                         ESP_LOGE(TAG,"Still setting gateway time. Ignoring packet...");
                     } else {
-                        parse_data(sr.bda, now, sr.ble_adv);
+                        parse_data(sr.bda, tv, sr.ble_adv);
                     }
                 }
             }
